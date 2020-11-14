ffa35ce65e702ae4cc9aec3fb6749bec2aff7b71
ENES FIRAT <74313028+enesfiratt44@users.noreply.github.com>
2020-11-14 16:37:34 +0300

    Update ilkform.Designer.cs

--- panel/ilkform.Designer.cs
+++ panel/ilkform.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -39,29 +38,21 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(768, 85);
+            this.label1.Size = new System.Drawing.Size(615, 69);
             this.label1.TabIndex = 0;
             this.label1.Text = "MENÜ TEST EKRANI";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(346, 144);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(226, 177);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // ilkform
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Maroon;
-            this.ClientSize = new System.Drawing.Size(891, 522);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(668, 424);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ilkform";
             this.Text = "ilkform";
             this.ResumeLayout(false);
@@ -72,6 +63,5 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
     }
 }
